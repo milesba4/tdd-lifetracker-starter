34410bc772b98d6114ba1cb1493d964ebdd1d35b
milesba4 <milesbaffour@gmail.com>
2022-06-29 14:43:47 -0400

    importing LandingPage in app.jsx file

--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "components/Navbar/Navbar";
+import LandingPage from "components/LandingPage/LandingPage";
 export default function App() {
   return (
     <div className="app">
@@ -11,8 +12,8 @@ export default function App() {
             <main>
               <Navbar />
               <Routes>
-                {/* <Route path="/" element={<LandingPage />} />
-                <Route path="/login" element={<LoginPage />} />
+                <Route path="/" element={<LandingPage />} />
+                {/* <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegistrationPage />} />
                 <Route path="/activity" element={<ActivityPage />} />
                 <Route path="/nutrition/*" element={<NutritionPage/>}/> */}
