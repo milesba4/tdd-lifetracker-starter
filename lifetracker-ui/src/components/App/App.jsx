c76884f4a990238c74bd036fb26d2709e682fc3f
milesba4 <milesbaffour@gmail.com>
2022-06-29 14:21:20 -0400

    importing navbar into app.jsx

--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "components/Navbar/Navbar";
 export default function App() {
   return (
     <div className="app">
@@ -10,11 +11,11 @@ export default function App() {
             <main>
               <Navbar />
               <Routes>
-                <Route path="/" element={<Landing />} />
+                {/* <Route path="/" element={<Landing />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegistrationPage />} />
                 <Route path="/activity" element={<ActivityPage />} />
-                <Route path="/nutrition/*" element={<NutritionPage/>}/>
+                <Route path="/nutrition/*" element={<NutritionPage/>}/> */}
               </Routes>
             </main>
           </BrowserRouter>
