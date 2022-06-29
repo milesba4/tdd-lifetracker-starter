fad826f935f5e3dd6749f02e9fbd68e216291c73
milesba4 <milesbaffour@gmail.com>
2022-06-29 15:13:14 -0400

    rendering loginForm component if user isnt logged in

--- lifetracker-ui/src/components/LoginPage/LoginPage.jsx
+++ lifetracker-ui/src/components/LoginPage/LoginPage.jsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import "./LoginPage.css";
-
+import LoginForm from "components/LoginForm/LoginForm";
 
 export default function LoginPage({isLoggedIn, setIsLoggedIn}){
     return(
-        <div className = "login-page">
-        {isLoggedIn? null :(<LoginForm/>)}
+        <div className = "login-page"> 
+        {isLoggedIn? <div> You clicked the login button</div> :(<LoginForm />)}  
         </div>
 
 
