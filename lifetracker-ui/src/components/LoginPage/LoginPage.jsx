ddbc8f0d467a801b94e0a06450bf32a5a73dc567
milesba4 <milesbaffour@gmail.com>
2022-06-29 15:59:30 -0400

    passing isLoggedIn function LoginForm component

--- lifetracker-ui/src/components/LoginPage/LoginPage.jsx
+++ lifetracker-ui/src/components/LoginPage/LoginPage.jsx
@@ -2,11 +2,10 @@ import * as React from "react";
 import "./LoginPage.css";
 import LoginForm from "components/LoginForm/LoginForm";
 
-export default function LoginPage({isLoggedIn, setIsLoggedIn}){
 export default function LoginPage({isLoggedIn, setIsLoggedIn, handleLoggedIn}){
     return(
         <div className = "login-page"> 
-        {isLoggedIn? <div> You clicked the login button</div> :(<LoginForm />)}  
+        {isLoggedIn? <div> You clicked the login button</div> :(<LoginForm handleLoggedIn = {handleLoggedIn}/>)}  
         </div>
 
 
