cd6aaf86823982939eeed1c1f181526907eafe0c
milesba4 <milesbaffour@gmail.com>
2022-06-29 13:07:34 -0400

    putting browserRouter inside context provider

--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -4,7 +4,17 @@ import "./App.css"
 export default function App() {
   return (
     <div className="app">
-      <React.Fragment>{/* YOUR CODE HERE! */}</React.Fragment>
+      <React.Fragment>{
+       <BrowserRouter>
+         <main>
+
+
+
+
+
+         </main>
+       </BrowserRouter> 
+        }</React.Fragment>
     </div>
   )
 }
