0a979a78af6a6f00587839a52c4ca328da224080
milesba4 <milesbaffour@gmail.com>
2022-06-29 18:14:37 -0400

    If user isn't logged in display registration form

--- lifetracker-ui/src/components/RegistrationPage/RegistrationPage.jsx
+++ lifetracker-ui/src/components/RegistrationPage/RegistrationPage.jsx
@@ -3,7 +3,8 @@ import "./RegistrationPage.css";
 import RegistrationForm from "components/RegistrationForm/RegistrationForm";
 export default function RegistrationPage({isLoggedIn}) {
   return (
-    <div className="Register">
+    <div className="registration-page">
+    {isLoggedIn? <div>You are logged in already</div> :(<RegistrationForm/>)} 
       
     </div>
   );
