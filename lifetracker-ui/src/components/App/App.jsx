b9de17359c26d318c39e01b1343d7ae8c56fd7ea
milesba4 <milesbaffour@gmail.com>
2022-06-29 13:08:08 -0400

    Importing browserRouter, routes, and route components

--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import "./App.css"
-
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 export default function App() {
   return (
     <div className="app">
