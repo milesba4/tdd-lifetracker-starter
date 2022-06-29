d6775052652e980a7ed1c1a56e2f00e92b606965
milesba4 <milesbaffour@gmail.com>
2022-06-29 14:22:15 -0400

    creating links in navbar

--- lifetracker-ui/src/components/NavLinks/NavLinks.jsx
+++ lifetracker-ui/src/components/NavLinks/NavLinks.jsx
@@ -2,4 +2,32 @@ import * as React from "react"
 import {Link } from "react-router-dom";
 
 export default function NavLinks() {
+    return(
+        <div className="nav-links">
+        <ul className="links">
+        <li>
+        <Link to="/activity"/>
+        Activity
+        </li>
+        <li>
+        <Link to="/exercise"/> Exercise
+        </li>
+        <li>
+        <Link to="/nutrition"/> Nutrition
+        </li>
+        <li>
+        <Link to="/sleep"/> Sleep
+        </li>
+        <li>
+        <Link to="/login"/> Login
+        </li>
+        <li className="btn secondary">
+          <a href="/register">Sign Up</a>
+        </li>
+      </ul>
+    </div>
+
+
+
+    )
 }
