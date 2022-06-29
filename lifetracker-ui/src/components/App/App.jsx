260170e63f082d1b96fb7897138b609910ecf484
milesba4 <milesbaffour@gmail.com>
2022-06-29 13:17:45 -0400

    Update App.jsx

--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
          <Navbar />
         <Routes>
         <Route path="/" element={<Landing/>}/> 
-        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/login" element={<LoginPage/>}/>
         <Route/>
         <Route/>
 
