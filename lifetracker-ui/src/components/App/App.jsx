3b45195bdb5409bc9296415c2905582462a2143b
milesba4 <milesbaffour@gmail.com>
2022-06-29 13:19:08 -0400

    Linking "/register" route to RegistrationPage component

--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -11,6 +11,7 @@ export default function App() {
         <Routes>
         <Route path="/" element={<Landing/>}/> 
         <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element={<RegistrationPage/>}/>
         <Route/>
         <Route/>
 
