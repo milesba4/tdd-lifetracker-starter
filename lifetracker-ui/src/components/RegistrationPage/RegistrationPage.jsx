641bc8304431c70804804559fb098507982c2530
milesba4 <milesbaffour@gmail.com>
2022-06-29 18:08:42 -0400

    import RegistrationForm inside RegistrationPage component

--- lifetracker-ui/src/components/RegistrationPage/RegistrationPage.jsx
+++ lifetracker-ui/src/components/RegistrationPage/RegistrationPage.jsx
@@ -1,77 +1,10 @@
 import * as React from "react";
 import "./RegistrationPage.css";
-
+import RegistrationForm from "components/RegistrationForm/RegistrationForm";
 export default function RegistrationPage({}) {
   return (
     <div className="Register">
-      <div className="card">
-        <h2>Register</h2>
-        <br></br>
-        <div className="registration-form">
-          <div className="form-input">
-            <label for="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              placeholder="Enter a valid email"
-              value=""
-            ></input>
-          </div>
-          <div className="form-input">
-            <label for="username">Username</label>
-            <input
-              type="text"
-              name="username"
-              placeholder="your_username"
-              value=""
-            ></input>
-          </div>
-          <div className="split-form-input">
-            <div className="form-input">
-              <input
-                type="text"
-                name="firstName"
-                placeholder="First Name"
-                value=""
-              ></input>
-            </div>
-            <div className="form-input">
-              <input
-                type="text"
-                name="lastName"
-                placeholder="Last Name"
-                value=""
-              ></input>
-            </div>
-          </div>
-          <div className="form-input">
-            <label for="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Enter a secure password"
-              value=""
-            ></input>
-          </div>
-          <div className="form-input">
-            <label for="passwordConfirm">Confirm Password</label>
-            <input
-              type="password"
-              name="passwordConfirm"
-              placeholder="Confirm your password"
-              value=""
-            ></input>
-          </div>
-          <button className="submit-registration" style="">
-            Create Account
-          </button>
-        </div>
-        <div className="footer">
-          <p>
-            Already have an account? Login <a href="/login">here</a>
-          </p>
-        </div>
-      </div>
+      
     </div>
   );
 }
