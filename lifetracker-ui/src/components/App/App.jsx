d37445d67b81a0898fe22f0acf4d2cc99e19d379
milesba4 <milesbaffour@gmail.com>
2022-06-29 15:22:10 -0400

    passing handleLogginIn function to LoginPage component

--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -21,7 +21,7 @@ export default function App() {
               <Navbar />
               <Routes>
                 <Route path="/" element={<LandingPage />} />
-                <Route path="/login" element={<LoginPage isLoggedIn = {isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+                <Route path="/login" element={<LoginPage isLoggedIn = {isLoggedIn} handleLoggedIn={handleLoggedIn}/>} />
                 {/* <Route path="/register" element={<RegistrationPage />} />
                 <Route path="/activity" element={<ActivityPage />} />
                 <Route path="/nutrition/*" element={<NutritionPage/>}/> */}
--- lifetracker-ui/src/components/LoginPage/LoginPage.jsx
+++ lifetracker-ui/src/components/LoginPage/LoginPage.jsx
@@ -3,6 +3,7 @@ import "./LoginPage.css";
 import LoginForm from "components/LoginForm/LoginForm";
 
 export default function LoginPage({isLoggedIn, setIsLoggedIn}){
+export default function LoginPage({isLoggedIn, setIsLoggedIn, handleLoggedIn}){
     return(
         <div className = "login-page"> 
         {isLoggedIn? <div> You clicked the login button</div> :(<LoginForm />)}  
