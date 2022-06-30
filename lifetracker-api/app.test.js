2e9e8e2609455819f5017fccf961aba0b9824c13
milesba4 <milesbaffour@gmail.com>
2022-06-30 14:10:03 -0400

    adding code to server.js

--- lifetracker-api/app.test.js
+++ lifetracker-api/app.test.js
@@ -1,19 +1,19 @@
-const express = require("express")
-const app = express()
-const morgan = require("morgan")
-// const{NotFoundError}= require("./utils/errors")
-// const storeRouter = require("./routes/store")
-// app.use("/store", storeRouter);
+// const express = require("express")
+// const app = express()
+// const morgan = require("morgan")
+// // const{NotFoundError}= require("./utils/errors")
+// // const storeRouter = require("./routes/store")
+// // app.use("/store", storeRouter);
 
-app.use(morgan("tiny"))
-app.use(express.json())
-
-
-
-app.get('/', (req, res) => {
-    res.status(200).send({ping:"pong"})
-  })
+// app.use(morgan("tiny"))
+// app.use(express.json())
 
 
 
-  module.exports=app // exporting app as default
+// app.get('/', (req, res) => {
+//     res.status(200).send({ping:"pong"})
+//   })
+
+
+
+//   module.exports=app // exporting app as default
