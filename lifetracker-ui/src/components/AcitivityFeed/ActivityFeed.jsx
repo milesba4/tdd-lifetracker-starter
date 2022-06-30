31554b6ba20d17492596603a37750ada6f924897
milesba4 <milesbaffour@gmail.com>
2022-06-30 17:13:45 -0400

    rendering activity feed in activity feed component

--- lifetracker-ui/src/components/AcitivityFeed/ActivityFeed.jsx
+++ lifetracker-ui/src/components/AcitivityFeed/ActivityFeed.jsx
@@ -3,5 +3,19 @@ import "./ActivityFeed.css";
 
 
 
-export default function ActivityFeed({}){
+export default function ActivityFeed({totalCaloriesPerDay,avgCaloriesPerCategory}){
+    return(
+    <div class="activity-feed">
+        <h2 class="heading">Activity Feed</h2>
+        <div className = "per-category">
+        <h4>Average Calories Per Category</h4>
+      
+
+        </div> 
+    </div>
+
+
+
+
+)
 }
