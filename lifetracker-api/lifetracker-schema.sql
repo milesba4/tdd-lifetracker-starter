dfed772acf0a3dc683d7ae395c439fe72e1840db
milesba4 <milesbaffour@gmail.com>
2022-07-01 12:23:25 -0400

    fixing schema

--- lifetracker-api/lifetracker-schema.sql
+++ lifetracker-api/lifetracker-schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE users(
     email    TEXT NOT NULL UNIQUE CHECK (POSITION('@' IN email) > 1),
     created_at  TIMESTAMP DEFAULT NOW (),
     updated_at TIMESTAMP DEFAULT NOW()
-)
+);
 
 CREATE TABLE nutrition(
     id      SERIAL PRIMARY KEY, 
@@ -16,6 +16,7 @@ CREATE TABLE nutrition(
     category    TEXT NOT NULL,
     calories INTEGER NOT NULL,
     image_url TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id),
     created_at  TIMESTAMP DEFAULT NOW ()
 )
