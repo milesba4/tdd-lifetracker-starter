9584a17761599e38f0a41f25327945e18a0d8968
milesba4 <milesbaffour@gmail.com>
2022-07-01 14:42:21 -0400

    rendering nutritionOverview component

--- lifetracker-ui/src/components/NutritionOverview/NutritionOverview.jsx
+++ lifetracker-ui/src/components/NutritionOverview/NutritionOverview.jsx
@@ -3,5 +3,15 @@ import * as React from "react";
 import "./NutritionOverview.css";
 
 export default function NutritionOverview({}) {
+    return (
+        <div class="nutrition-overview">
+        <div class="header">
+          <h3>Overview</h3>
+          <Link to="/nutrition/create"> <button class="Button outline small outline aqua ">
+            Record Nutrition
+          </button> </Link>
+        </div>
+       <NutritionFeed/>
+      </div>
     );
   }
