1a6e77b42302cebab110bb5004a301c5f567ab84
milesba4 <milesbaffour@gmail.com>
2022-07-11 15:06:55 -0400

    creating security middleware

--- lifetracker-api/middleware/security.js
+++ lifetracker-api/middleware/security.js
@@ -1,59 +1,53 @@
-const jwt = require("jsonwebtoken")
-const {SECRET_KEY} = require("../config")
-const {UnauthorizedError} = require("../utils/errors")
-
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+const { UnauthorizedError } = require("../utils/errors");
 
 //create function to extract jwt from req header
 
 //Authorization: Bearer oneonqdonqwdinwqoin;
-const jwtFrom = ({headers}) => {
-if (headers?.authorization){
-const [scheme, token] = headers.authorization.split(" ")
-if(scheme.trim()=== "Bearer"){
-    return token
-
-
-}
-}
-return undefined
-}
-
+const jwtFrom = ({ headers }) => {
+  if (headers?.authorization) {
+    const [scheme, token] = headers.authorization.split(" ");
+    if (scheme.trim() === "Bearer") {
+      return token;
+    }
+  }
+  return undefined;
+};
 
 //create function to attach user to the res object
 const extractUserFromJwt = (req, res, next) => {
-try{
-const token = jwtFrom(req)
-if(token){
-    res.locals.user = jwt.verify(token, SECRET_KEY)
-}
-return next()
-}catch(err){
-return next()
-
-}
-
-
-}
+  try {
+    const token = jwtFrom(req);
+    if (token) {
+      res.locals.user = jwt.verify(token, SECRET_KEY);
+      console.log("jwt:", jwt.verify(token, SECRET_KEY));
+      console.log("yo,", res.locals.user);
+    }
+    return next();
+  } catch (err) {
+    return next();
+  }
+};
 
 //create function to verify that an authed user exists
 
-const verifyAuthUser= (req,res,next)=>{
-
-try{
-const{user} = res.locals
-if(user?.email){
-throw new UnauthorizedError()
-}
-return next()
-}catch(err){
-return next(err)
-
-}
-    
-}
+const verifyAuthUser = (req, res, next) => {
+  try {
+    console.log("res.locals=", res.locals);
+    const {user}   = res.locals;
+    console.log("usetr=", user);
+    if (!user?.email) {
+      throw new UnauthorizedError();
+    }
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+};
 
 module.exports = {
-    jwtFrom,
-    verifyAuthUser,
-    extractUserFromJwt
-    }
+  jwtFrom,
+  verifyAuthUser,
+  extractUserFromJwt,
+};
