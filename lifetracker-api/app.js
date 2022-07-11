d8fbfd8b0512c17bd6c91063e0927e21529629e8
milesba4 <milesbaffour@gmail.com>
2022-07-11 15:06:40 -0400

    Update app.js

--- lifetracker-api/app.js
+++ lifetracker-api/app.js
@@ -1,28 +1,29 @@
 const express = require("express")
 const { NotFoundError} = require("./utils/errors")
+const nutritionRoutes = require("./routes/nutrition")
 const authRoutes = require("./routes/auth")
 const morgan = require("morgan")
 const cors = require("cors")
 const app = express()
 app.use(cors())
 const security = require("./middleware/security")
-
-
-
-//parse incoming request bodies with JSON payload
+app.use(morgan("tiny"))
 app.use(express.json())
+app.use(security.extractUserFromJwt)
+//parse incoming request bodies with JSON payload
+
 
 // log request info 
-app.use(morgan("tiny"))
+
 
 //for every request check if a token exists in auth header
   // if it does attach decoded user to res.locals
-app.use(security.extractUserFromJwt)
+
 
 
 
 app.use("/auth", authRoutes)
-
+app.use("/nutrition", nutritionRoutes)
 //test GET request
 app.get("/",(req, res, next) => {
     res.status(200).json({"ping":"pong"})
