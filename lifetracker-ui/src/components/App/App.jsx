c035994028eb08c536e5684bf95b43fa59a6e259
milesba4 <milesbaffour@gmail.com>
2022-07-11 15:07:20 -0400

    Update App.jsx

--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -1,21 +1,34 @@
 import * as React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {AuthContextProvider,useAuthContext } from "../../contexts/auth"
+import apiClient from "../../services/apiClient";
 import Navbar from "components/Navbar/Navbar";
 import LandingPage from "components/LandingPage/LandingPage";
 import LoginPage from "components/LoginPage/LoginPage";
 import RegistrationPage from "components/RegistrationPage/RegistrationPage";
-export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+import ActivityPage from "components/ActivityPage/ActivityPage";
+import NutritionPage from "components/NutritionPage/NutritionPage";
+import NutritionNew from "components/NutritionNew/NutritionNew";
+import { NutritionContextProvider } from "../../contexts/nutrition"
+export default function AppContainer(){
+return(
+    <AuthContextProvider>
+        <NutritionContextProvider>
+          <App />
+        </NutritionContextProvider>
+      </AuthContextProvider>
+)
+}
+
+function App() {
+  const {user} = useAuthContext()
+  const {errors, setErrors} = useAuthContext()
+
   const[loginForm,setLoginForm] = React.useState({email:"",password:""})
   const[registrationForm, setRegistrationForm]=React.useState({email:"",username:"",firstName:"", lastName:"",password:"", passwordConfirm:""})
 
 
-  function handleLoggedIn(evt){
-    setIsLoggedIn(true)
-  }
-  
- 
 
   return (
     <div className="app">
@@ -26,10 +39,11 @@ export default function App() {
               <Navbar />
               <Routes>
                 <Route path="/" element={<LandingPage />} />
-                <Route path="/login" element={<LoginPage loginForm = {loginForm} setLoginForm = {setLoginForm} isLoggedIn = {isLoggedIn} handleLoggedIn={handleLoggedIn}/>} />
-                <Route path="/register" element={<RegistrationPage registrationForm={registrationForm} setRegistrationForm={setRegistrationForm} isLoggedIn = {isLoggedIn}/>} />
-                {/* <Route path="/activity" element={<ActivityPage />} />
-                <Route path="/nutrition/*" element={<NutritionPage/>}/> */}
+                <Route path="/login" element={<LoginPage errors = {errors} setErrors = {setErrors} loginForm = {loginForm} setLoginForm = {setLoginForm} />} />
+                <Route path="/register" element={<RegistrationPage errors = {errors} setErrors={setErrors} registrationForm={registrationForm} setRegistrationForm={setRegistrationForm}/>} />
+                <Route path="/activity" element={<ActivityPage />} />
+                <Route path="/nutrition/*" element={<NutritionPage />}/>
+                <Route path="/nutrition/create" element={<NutritionNew />}/>
               </Routes>
             </main>
           </BrowserRouter>
